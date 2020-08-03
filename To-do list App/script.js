2bfa923fdb941d63ca059d26f55e19e3fb50f519
Bougherra khaled <63866410+khaled-bougui@users.noreply.github.com>
2020-08-04 00:42:28 +0100

    first commit

---
+++ To-do list App/script.js
@@ -0,0 +1,20 @@
+var addItemBtn = document.querySelector(".Add-item");
+var taskBox = document.getElementsByClassName("taskBox");
+var taskInput = document.getElementsByClassName("taskInput");
+
+addItemBtn.addEventListener("click",function(){
+    
+    taskBox.item(0).classList.remove("delete");
+    taskInput.item(0).style.display = "initial";
+});
+
+
+//for the delete :
+/*
+var deleteBtn = document.getElementsByClassName("deleteBtn");
+deleteBtn.addEventListener("click",function(){
+    taskBox.item(0).className += "delete";
+    taskInput.item(0).style.display = "none";
+})
+*/
+//how to remove btns border ???
